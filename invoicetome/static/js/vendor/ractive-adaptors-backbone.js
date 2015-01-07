ab54c5fc3465c52e2a9f7dbf81d36cd58a2bc9d0
zaebee <zaebuntu@gmail.com>
2015-01-07 19:57:42 +0400

    moved js libs into vendor dir

--- invoicetome/static/js/vendor/ractive-adaptors-backbone.js
+++ invoicetome/static/js/vendor/ractive-adaptors-backbone.js
@@ -3,7 +3,7 @@
 	Backbone adaptor plugin
 	=======================
 
-	Version 0.1.0. Copyright 2013 @rich_harris, MIT licensed.
+	Version 0.2.0. Copyright 2013 - 2014 @rich_harris, MIT licensed.
 
 	This plugin allows Ractive.js to work seamlessly with Backbone.Model and
 	Backbone.Collection instances.
@@ -15,32 +15,32 @@
 
 	Troubleshooting: If you're using a module system in your app (AMD or
 	something more nodey) then you may need to change the paths below,
-	where it says `require( 'ractive' )` or `define([ 'Ractive' ]...)`.
+	where it says `require( 'ractive' )` or `define([ 'ractive' ]...)`.
 
 	==========================
 
 	Usage: Include this file on your page below Ractive, e.g:
 
-	    <script src='lib/Ractive.js'></script>
-	    <script src='lib/Ractive-adaptors-Backbone.js'></script>
+	    <script src='lib/ractive.js'></script>
+	    <script src='lib/ractive-adaptors-backbone.js'></script>
 
 	Or, if you're using a module loader, require this module:
 
 	    define( function ( require ) {
-	      var Ractive = require( 'Ractive' );
+	      var Ractive = require( 'ractive' );
 
 	      // requiring the plugin will 'activate' it - no need to use
 	      // the return value
-	      require( 'Ractive-adaptors-Backbone' );
+	      require( 'ractive-adaptors-backbone' );
 	    });
 
-	Then tell Ractive to expect Backbone objects by adding an `adaptors` property:
+	Then tell Ractive to expect Backbone objects by adding an `adapt` property:
 
 	    var ractive = new Ractive({
 	      el: myContainer,
 	      template: myTemplate,
 	      data: { foo: myBackboneModel, bar: myBackboneCollection },
-	      adaptors: [ 'Backbone' ]
+	      adapt: [ 'Backbone' ]
 	    });
 
 */
@@ -51,12 +51,12 @@
 
 	// Common JS (i.e. browserify) environment
 	if ( typeof module !== 'undefined' && module.exports && typeof require === 'function' ) {
-		factory( require( 'Ractive' ), require( 'backbone' ) );
+		factory( require( 'ractive' ), require( 'backbone' ) );
 	}
 
 	// AMD?
 	else if ( typeof define === 'function' && define.amd ) {
-		define([ 'Ractive', 'Backbone' ], factory );
+		define([ 'ractive', 'backbone' ], factory );
 	}
 
 	// browser global
@@ -65,17 +65,31 @@
 	}
 
 	else {
-		throw new Error( 'Could not find Ractive or Backbone! Both must be loaded before the Ractive-adaptors-Backbone plugin' );
+		throw new Error( 'Could not find Ractive or Backbone! Both must be loaded before the ractive-adaptors-backbone plugin' );
 	}
 
 }( typeof window !== 'undefined' ? window : this, function ( Ractive, Backbone ) {
 
 	'use strict';
 
-	var BackboneModelWrapper, BackboneCollectionWrapper;
+	var BackboneModelWrapper, BackboneCollectionWrapper, lockProperty = '_ractiveAdaptorsBackboneLock';
 
 	if ( !Ractive || !Backbone ) {
 		throw new Error( 'Could not find Ractive or Backbone! Check your paths config' );
+	}
+
+	function acquireLock( key ) {
+		key[lockProperty] = ( key[lockProperty] || 0 ) + 1;
+		return function release() {
+			key[lockProperty] -= 1;
+			if ( !key[lockProperty] ) {
+				delete key[lockProperty];
+			}
+		};
+	}
+
+	function isLocked( key ) {
+		return !!key[lockProperty];
 	}
 
 	Ractive.adaptors.Backbone = {
@@ -92,14 +106,12 @@
 	};
 
 	BackboneModelWrapper = function ( ractive, model, keypath, prefix ) {
-		var wrapper = this;
-
 		this.value = model;
 
 		model.on( 'change', this.modelChangeHandler = function () {
-			wrapper.setting = true;
+			var release = acquireLock( model );
 			ractive.set( prefix( model.changed ) );
-			wrapper.setting = false;
+			release();
 		});
 	};
 
@@ -113,33 +125,32 @@
 		set: function ( keypath, value ) {
 			// Only set if the model didn't originate the change itself, and
 			// only if it's an immediate child property
-			if ( !this.setting && keypath.indexOf( '.' ) === -1 ) {
+			if ( !isLocked( this.value ) && keypath.indexOf( '.' ) === -1 ) {
 				this.value.set( keypath, value );
 			}
 		},
 		reset: function ( object ) {
 			// If the new object is a Backbone model, assume this one is
 			// being retired. Ditto if it's not a model at all
-			if ( object instanceof Backbone.Model || !( object instanceof Object ) ) {
+			if ( object instanceof Backbone.Model || !(object instanceof Object) ) {
 				return false;
 			}
 
 			// Otherwise if this is a POJO, reset the model
-			this.value.clear().set( object );
+			//Backbone 1.1.2 no longer has reset and just uses set
+			this.value.set( object );
 		}
 	};
 
 	BackboneCollectionWrapper = function ( ractive, collection, keypath ) {
-		var wrapper = this;
-
 		this.value = collection;
 
 		collection.on( 'add remove reset sort', this.changeHandler = function () {
 			// TODO smart merge. It should be possible, if awkward, to trigger smart
 			// updates instead of a blunderbuss .set() approach
-			wrapper.setting = true;
+			var release = acquireLock( collection );
 			ractive.set( keypath, collection.models );
-			wrapper.setting = false;
+			release();
 		});
 	};
 
@@ -151,7 +162,7 @@
 			return this.value.models;
 		},
 		reset: function ( models ) {
-			if ( this.setting ) {
+			if ( isLocked( this.value ) ) {
 				return;
 			}
 
