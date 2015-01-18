12dccca364d37a761a3ab6ffc45ae7a62dfae0de
zaebee <zaebuntu@gmail.com>
2015-01-18 11:10:07 +0300

    added InvoiceAdmin

--- invoicetome/apps/invoice/admin.py
+++ invoicetome/apps/invoice/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 from .models import Invoice
 
 
-admin.site.register(Invoice)
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ('__unicode__', 'uuid', 'owner', 'email', 'status', 'signed', 'date_added')
+    list_filter = ('owner', 'status')
+
+
+admin.site.register(Invoice, InvoiceAdmin)
