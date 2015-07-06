f380f0252736c8369570b4af37976decca8c54c7
zaebee <zaebuntu@gmail.com>
2015-07-06 11:04:32 +0000

    changed locales

--- invoicetome/settings.py
+++ invoicetome/settings.py
@@ -116,7 +116,7 @@ TEMPLATE_DIRS = (
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 LANGUAGES = (
     ('ru', ugettext('rus')),
